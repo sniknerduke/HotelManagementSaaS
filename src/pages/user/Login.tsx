3b3d06004b925e3345c89d1e497e23b12dc5c271
NguyenDangKhoi <khoisever1234@gmail.com>
2026-05-28 21:10:57 +0700

    final 2

--- src/pages/user/Login.tsx
+++ src/pages/user/Login.tsx
@@ -243,7 +243,7 @@ export const Login: React.FC = () => {
                   <path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" />
                   <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" />
                 </svg>
-                <span className="text-[10px] uppercase tracking-[0.2em] font-bold">{t('login.google')}</span>
+                <span className="text-[10px] uppercase tracking-[0.2em] font-bold">{t('login.gg')}</span>
               </button>
 
               <button 
@@ -255,7 +255,7 @@ export const Login: React.FC = () => {
                   <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.469h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.469h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
                   <path fill="#ffffff" d="M15.83 12.073v-2.25c0-.949.465-1.874 1.956-1.874h2.25v-2.953s-1.374-.235-2.686-.235c-2.741 0-4.533 1.662-4.533 4.669v3.469h-3.047v3.469h3.047v8.385A12.09 12.09 0 0 0 16 23.927v-8.385h2.796l.532-3.469H15.83z" />
                 </svg>
-                <span className="text-[10px] uppercase tracking-[0.2em] font-bold">{t('login.facebook')}</span>
+                <span className="text-[10px] uppercase tracking-[0.2em] font-bold">{t('login.fb')}</span>
               </button>
             </div>
           </form>
